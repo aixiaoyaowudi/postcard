26e8cea072b019e4d6f0212f2939b0e58754f342
Ziyao Xiao <aixiaoyaowudi@gmail.com>
2023-06-23 11:09:49 +0800

    2023.6.23 11:09 update

--- compile.py
+++ compile.py
@@ -3,6 +3,7 @@
 import json
 import re
 import os
+import argparse
 
 def upper_text(ch):
 
@@ -28,28 +29,40 @@ def lower_text(ch):
     
     return ret
 
-with open("./config.json","r") as f:
-    data = json.load(f)
+if __name__ == '__main__':
 
-with open("./template.tex","r") as f:
-    template = f.read()
+    parser = argparse.ArgumentParser(description = 'Postcard Compiler')
 
-compile_command = "xelatex -file-line-error -halt-on-error -interaction=nonstopmode -output-directory=./temp %s > /dev/null"
-convert_command = "convert -density 600 -alpha remove %s %s > /dev/null"
+    parser.add_argument('-c', '--character', default = '')
 
-for ch, _ in data.items():
-    print("Processing %s" % ch)
-    output = re.sub("UPPER_LEFT_TEXT", upper_text(ch), template)
-    output = re.sub("LOWER_RIGHT_TEXT", lower_text(ch), output)
-    output = re.sub("ALPHABET", r"\\mathcal{%s}" % ch, output)
-    output = re.sub("SCALE_SIZE", str(_["scale"]), output)
-    output = re.sub("FORMULA", _["formula"], output)
-    output = re.sub("FORM_NAME", _["formula_name"], output)
-    with open("./temp/%s.tex" % ch, "w") as f:
-        f.write(output)
-    
-    print("Generating PDF")
-    os.system(compile_command % ("./temp/%s.tex" % ch))
+    args = parser.parse_args()
 
-    print("Generating Image")
-    os.system(convert_command % ("./temp/%s.pdf" % ch, "./output/%s.png" % ch))
+    with open("./config.json","r") as f:
+        data = json.load(f)
+
+    with open("./template.tex","r") as f:
+        template = f.read()
+
+    compile_command = "xelatex -file-line-error -halt-on-error -interaction=nonstopmode -output-directory=./temp %s > /dev/null"
+    convert_command = "convert -density 600 -alpha remove %s %s > /dev/null"
+
+    for ch, _ in data.items():
+
+        if args.character != '' and args.character != ch:
+            continue
+
+        print("Processing %s" % ch)
+        output = re.sub("UPPER_LEFT_TEXT", upper_text(ch), template)
+        output = re.sub("LOWER_RIGHT_TEXT", lower_text(ch), output)
+        output = re.sub("ALPHABET", r"\\mathcal{%s}" % ch, output)
+        output = re.sub("SCALE_SIZE", str(_["scale"]), output)
+        output = re.sub("FORMULA", _["formula"], output)
+        output = re.sub("FORM_NAME", _["formula_name"], output)
+        with open("./temp/%s.tex" % ch, "w") as f:
+            f.write(output)
+        
+        print("Generating PDF")
+        os.system(compile_command % ("./temp/%s.tex" % ch))
+
+        print("Generating Image")
+        os.system(convert_command % ("./temp/%s.pdf" % ch, "./output/%s.png" % ch))
